ad2831ec76496c8cb44f62c19a284fcabc9765a5
pablito <pablomota9955@gmail.com>
2026-02-12 11:23:16 +0000

    Unificando pastas app e src

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -361,7 +361,7 @@ export default function LoginPage() {
               <p className="text-sm text-gray-600">
                 Ainda não tem conta?{' '}
                 <Link href="/cadastro" className="text-blue-600 hover:text-blue-700 font-semibold hover:underline">
-                  Criar conta gratuita
+                  TESTE RENDER
                 </Link>
               </p>
             </div>
